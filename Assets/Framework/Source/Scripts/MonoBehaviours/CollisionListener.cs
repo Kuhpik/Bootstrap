445bfcbfd6b8b94063108f06ce7c6a35eb57f31e
K'uh Pik <improvetheworld@yandex.ru>
2020-04-01 13:25:02 +0300

    Collision listeners added.

---
+++ Assets/Framework/Source/Scripts/MonoBehaviours/CollisionListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class CollisionListener : MonoBehaviour
+{
+    public Action<Transform> CollisionEnterEvent, CollisionExitEvent;
+    public Action<Transform> TriggerEnterEvent, TriggetExitEvent;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CollisionEnterEvent?.Invoke(collision.transform);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        CollisionExitEvent?.Invoke(collision.transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent?.Invoke(other.transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggetExitEvent?.Invoke(other.transform);
+    }
+}
---
+++ Assets/Framework/Source/Scripts/MonoBehaviours/CollisionListener2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class CollisionListener2D : MonoBehaviour
+{
+    public Action<Transform> CollisionEnterEvent2D, CollisionExitEvent2D;
+    public Action<Transform> TriggerEnterEvent2D, TriggetExitEvent2D;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CollisionEnterEvent2D?.Invoke(collision.transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        CollisionExitEvent2D?.Invoke(collision.transform);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TriggerEnterEvent2D?.Invoke(collision.transform);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        TriggetExitEvent2D?.Invoke(collision.transform);
+    }
+}
