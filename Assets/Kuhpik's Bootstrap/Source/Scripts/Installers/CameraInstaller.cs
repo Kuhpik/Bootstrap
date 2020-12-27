80487c73211bafe96943e0b1737701fad064968d
K'uh Pik <improvetheworld@yandex.ru>
2020-12-27 21:52:21 +0300

    Camera installer improvements

--- Assets/Kuhpik's Bootstrap/Source/Scripts/Installers/CameraInstaller.cs
+++ Assets/Kuhpik's Bootstrap/Source/Scripts/Installers/CameraInstaller.cs
@@ -1,4 +1,6 @@
 ﻿using NaughtyAttributes;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -8,38 +10,73 @@ namespace Kuhpik
 {
     public class CameraInstaller : MonoBehaviour
     {
-        [SerializeField] private bool scaleFOV;
-        [SerializeField] [ShowIf("scaleFOV")] private bool portrait;
-        [SerializeField] [ShowIf("scaleFOV")] private bool allCameras;
-        [SerializeField] [HideIf("allCameras")] private Camera[] cameras;
+        [SerializeField] bool scaleFOV;
+        [SerializeField] [ShowIf("scaleFOV")] bool allCameras;
+        [SerializeField] [ShowIf("scaleFOV")] bool clampAspectRatio;
+        [SerializeField] [HideIf("allCameras")] Camera[] cameras;
+        [SerializeField] [ShowIf("scaleFOV")] Vector2 targetResolution;
+        [SerializeField] [ShowIf("scaleFOV")] [Tooltip("I commonly use this for tablet devices")] Vector2 screenRatioClamp = Vector2.up;
 
+        Dictionary<Camera, float> fovDictionary;
+
+        /// <summary>
+        /// Initial setup and values preparations. Don't call manually from code.
+        /// </summary>
         public void Process()
         {
             if (scaleFOV)
             {
-                var screenSize = portrait ? new Vector2(1080, 1920) : new Vector2(1920, 1080);
-                var collection = allCameras ? FindObjectsOfType<Camera>() : cameras;
-                var screenAspect = (float)Screen.width / (float)Screen.height;
-                var aspect = screenSize.x / screenSize.y;
+                var cams = allCameras ? FindObjectsOfType<Camera>() : cameras;
+                fovDictionary = cams.ToDictionary(x => x, x => x.orthographic ? x.orthographicSize : x.fieldOfView);
+                Resize();
+            }
+        }
 
-                Debug.Log($"Current screen size is {Screen.width}x{Screen.height}. Aspect ratio is {screenAspect}");
-
-                foreach (var cam in collection)
+        /// <summary>
+        /// Adding Camera to resizing ones. Most common use case - instantiating cameras via systems.
+        /// </summary>
+        /// <param name="camera">Camera</param>
+        /// <param name="autoResize">Should camera's FOV be resized right after this method call</param>
+        public void AddCamera(Camera camera, bool autoResize = true)
+        {
+            if (scaleFOV)
+            {
+                if (fovDictionary.ContainsKey(camera))
                 {
-                    var camRads = (cam.orthographic ? cam.orthographicSize : cam.fieldOfView) * Mathf.Deg2Rad;
-                    camRads = 2f * Mathf.Atan(Mathf.Tan(camRads / 2f) * aspect);
-                    var screenRads = 2f * Mathf.Atan(Mathf.Tan(camRads / 2f) / screenAspect);
-
-                    if (!cam.orthographic)
-                    {
-                        cam.fieldOfView = screenRads * Mathf.Rad2Deg;
-                    }
-
-                    else
-                    {
-                        cam.orthographicSize = screenRads * Mathf.Rad2Deg;
-                    }
+                    Debug.Log("Camera was already added");
+                    return;
                 }
+
+                fovDictionary.Add(camera, camera.orthographic ? camera.orthographicSize : camera.fieldOfView);
+                if (autoResize) Resize();                
+            }
+        }
+
+        /// <summary>
+        /// Resize FOV method. Use this for specific cases like resize on screenshot.
+        /// </summary>
+        public void Resize()
+        {
+            if (!scaleFOV) 
+            { 
+                Debug.Log("Resize is disabled in Installer's settings"); 
+                return; 
+            }
+
+            var screenAspect = (float)Screen.width / (float)Screen.height;
+            var aspect = targetResolution.x / targetResolution.y;
+
+            if (clampAspectRatio) aspect = Mathf.Clamp(aspect, screenRatioClamp.x, screenRatioClamp.y);
+            Debug.Log($"Current screen size is {Screen.width}x{Screen.height}. Aspect ratio is {screenAspect}");
+
+            foreach (var cam in fovDictionary.Keys)
+            {
+                var fov = fovDictionary[cam];
+                var rads = 2f * Mathf.Atan(Mathf.Tan(fov * Mathf.Deg2Rad / 2f) * aspect);
+                var screenRads = 2f * Mathf.Atan(Mathf.Tan(rads / 2f) / screenAspect);
+
+                if (cam.orthographic) cam.orthographicSize = screenRads * Mathf.Rad2Deg; 
+                else cam.fieldOfView = screenRads * Mathf.Rad2Deg;
             }
         }
     }
