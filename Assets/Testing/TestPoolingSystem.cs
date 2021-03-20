737787c2f191dc5c96aadc6ff298a18166a94471
Kuhpik <improvetheworld@yandex.ru>
2021-03-20 20:43:27 +0300

    Pooling test

---
+++ Assets/Testing/TestPoolingSystem.cs
@@ -0,0 +1,37 @@
+﻿using Kuhpik;
+using Kuhpik.Pooling;
+using UnityEngine;
+
+public class TestPoolingSystem : GameSystem, IIniting, IUpdating
+{
+    [SerializeField] KeyCode restartKey;
+    [SerializeField] bool isTesting;
+
+    [Header("Pool")]
+    [SerializeField] GameObject[] prefabsToGet;
+    [SerializeField] GameObject[] prefabsToPool;
+
+    void IIniting.OnInit()
+    {
+        if (!isTesting) return;
+
+        foreach (var prefab in prefabsToGet)
+        {
+            PoolingSystem.GetObject(prefab);
+        }
+
+        foreach (var prefab in prefabsToPool)
+        {
+            var @object = PoolingSystem.GetObject(prefab);
+            PoolingSystem.Pool(@object);
+        }
+    }
+
+    void IUpdating.OnUpdate()
+    {
+        if (Input.GetKeyDown(restartKey))
+        {
+            Bootstrap.GameRestart(0);
+        }
+    }
+}
