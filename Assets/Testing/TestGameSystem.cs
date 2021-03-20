ef62ffc54b97f11dccf2f1152d3b2323c54f2cbc
Kuhpik <improvetheworld@yandex.ru>
2021-03-20 06:58:02 +0300

    Testing scene
    Won't go into .unitypackage

---
+++ Assets/Testing/TestGameSystem.cs
@@ -0,0 +1,17 @@
+﻿using Kuhpik;
+using UnityEngine;
+
+public class TestGameSystem : GameSystem, IIniting, IUpdating
+{
+    void IIniting.OnInit()
+    {
+        Debug.Log($"<color=yellow>Game state inited. Frame {Time.frameCount}</color>");
+    }
+
+    void IUpdating.OnUpdate()
+    {
+        Debug.Log($"Game state update. Frame {Time.frameCount}");
+
+        if (Input.GetKeyDown(KeyCode.Space)) Bootstrap.ChangeGameState(EGamestate.Loading);
+    }
+}
