cbe71014a515e0fceafdea7ee0d9a3f90880d465
Kuhpik <improvetheworld@yandex.ru>
2021-03-22 14:10:45 +0300

    Test systems for multi-states

---
+++ Assets/Testing/TestPauseSystem.cs
@@ -0,0 +1,15 @@
+﻿using Kuhpik;
+using UnityEngine;
+
+public class TestPauseSystem : GameSystem, IIniting, IUpdating
+{
+    void IIniting.OnInit()
+    {
+        Debug.Log($"<color=yellow>Pause state inited. Frame {Time.frameCount}</color>");
+    }
+
+    void IUpdating.OnUpdate()
+    {
+        Debug.Log($"Pause state update. Frame {Time.frameCount}");
+    }
+}
