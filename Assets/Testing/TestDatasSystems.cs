9c7d56fbac9ad786b5ae8a8f69ed5ca02a30884f
Kuhpik <improvetheworld@yandex.ru>
2021-03-24 12:58:19 +0300

    Datainjection test system :tired_face:

---
+++ Assets/Testing/TestDatasSystems.cs
@@ -0,0 +1,11 @@
+﻿using Kuhpik;
+using UnityEngine;
+
+public class TestDatasSystems : GameSystem, IIniting
+{
+    void IIniting.OnInit()
+    {
+        Debug.Log(player);
+        Debug.Log(game);
+    }
+}
