0453d3ce19570afe68570f11a60f27be7acb3b6f
Alexey Rodionov <improvetheworld@yandex.ru>
2021-12-14 12:11:49 +0300

    Add LogExtension to skip most of the logs for release

---
+++ Assets/Kuhpik's Bootstrap/Source/Scripts/Extensions/LogExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kuhpik.Extensions
+{
+    public static class LogExtensions
+    {
+        public static void Log(string message)
+        {
+            Log(message, Color.white);
+        }
+
+        public static void Log(string message, Color color)
+        {
+#if DEBUG
+            Debug.Log($"<color={ColorUtility.ToHtmlStringRGB(color)}>{message}</color>");
+#endif
+        }
+    }
+}
