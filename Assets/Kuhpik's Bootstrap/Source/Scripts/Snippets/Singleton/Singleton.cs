0af81b514c55450a4f8bb9668b196c45ae650424
Alexey Rodionov <improvetheworld@yandex.ru>
2022-05-15 00:37:44 +0300

    Small improvements for Singletons...
    They became a bit safer and informative

--- Assets/Kuhpik's Bootstrap/Source/Scripts/Snippets/Singleton/Singleton.cs
+++ Assets/Kuhpik's Bootstrap/Source/Scripts/Snippets/Singleton/Singleton.cs
@@ -8,15 +8,34 @@ namespace Kuhpik
 
         void Awake()
         {
-            if (Instance == null)
+            foreach (var instance in FindObjectsOfType(typeof(T), false))
             {
-                Instance = this as T;
+                if (instance != this)
+                {
+                    var go = (instance as T).gameObject;
+                    WarnAboutDuplicate(go);
+                    DestroyImmediate(go);
+                }
             }
 
-            else
-            {
-                DestroyImmediate(gameObject);
-            }
+            Instance = this as T;
+        }
+
+        // Just adding a bit more safety
+        // https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnApplicationQuit.html
+        void OnApplicationQuit()
+        {
+            Instance = null;
+        }
+
+        void WarnAboutDuplicate(GameObject instance)
+        {
+            Debug.LogError
+            (
+                $"Found duplicate Singleton in scene hierarchy. " +
+                $"Type of: {typeof(T).Name} " +
+                $"and GO name: {instance.name}!"
+            );
         }
     }
 }
