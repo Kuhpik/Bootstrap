dfa02bac37d7cc8ae4eaf382490e719342548780
Alexey Rodionov <improvetheworld@yandex.ru>
2022-05-15 02:21:13 +0300

    Added extension methods for direction calculations

--- Assets/Kuhpik's Bootstrap/Source/Scripts/Extensions/MathExtentions.cs
+++ Assets/Kuhpik's Bootstrap/Source/Scripts/Extensions/MathExtentions.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace Kuhpik.Extensions
 {
     public static class MathExtentions
@@ -6,6 +8,29 @@ namespace Kuhpik.Extensions
         public static float GetPercentOfValueInRange(float value, float min, float max)
         {
             return (value - min) / (max - min);
+        }
+
+        // https://docs.unity3d.com/2019.3/Documentation/Manual/DirectionDistanceFromOneObjectToAnother.html
+        public static Vector3 GetDirection(this Transform from, Transform to)
+        {
+            return to.position - from.position;
+        }
+
+        public static Vector3 GetDirectionNormalized(this Transform from, Transform to)
+        {
+            return GetDirection(from, to).normalized;
+        }
+
+        public static Vector3 GetDirectionIgnoreY(this Transform from, Transform to)
+        {
+            var direction = GetDirection(from, to);
+            direction.y = 0;
+            return direction;
+        }
+
+        public static Vector3 GetDirectionNormalizedIgnoreY(this Transform from, Transform to)
+        {
+            return GetDirectionIgnoreY(from, to).normalized;
         }
     }
 }
